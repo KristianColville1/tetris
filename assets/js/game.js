9c9a37ce3c110e80915ffd39b93915010060ed05
KristianColville1 <kristiancolville1@gmail.com>
2022-04-19 22:00:24 +0100

    Add doc strings and clean for loops for game grid cells

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,16 +2,25 @@ const gameGrid = document.getElementById('game');
 const gameContainers = document.getElementsByClassName('container-row');
 const startGame = document.getElementsByTagName('button');
 
+/**
+ * @name runGame
+ * @description Builds the game grid using HTML elements for the cells.
+ */
 function buildGameGrid(){
-    alert('active');
-    for(let i = 0; i < 15; i++){
+    for(var i = 0; i < 16; i++){
         gameGrid.innerHTML +='<div class="container-row"></div>\n';
-    }
-    for(let o = 0; o < 15; o++){
-        for(let i = 0; i < 18; i++){
-            gameContainers[o].innerHTML += '<div class="square"></div>\n';
+        for(var j = 0; j < 17; j++){
+            gameContainers[i].innerHTML += '<div class="square"></div>\n';
         }
     }
 }
 
-startGame[0].addEventListener('click', buildGameGrid);
+/**
+ * @name runGame
+ * @description Calls all the necessary functions to run the game.
+ */
+function runGame(){
+    buildGameGrid();
+}
+
+startGame[0].addEventListener('click', runGame);
